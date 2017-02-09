bf7bb743675f2cb342b5e81d1da1d1725e4cc8d8
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2017-02-09 09:46:46 +0100

    Clean up + converter

---
+++ TressMontage.Client/TressMontage.Client/Converters/FileTypeIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using TressMontage.Utilities;
+using Xamarin.Forms;
+
+namespace TressMontage.Client.Converters
+{
+    public class FileTypeIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DirectiveTypes)
+            {
+                var directiveType = (DirectiveTypes)value;
+
+                switch (directiveType)
+                { 
+                    case DirectiveTypes.Folder:
+                        return "icon_folder.png";
+                }
+
+                return string.Empty;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
